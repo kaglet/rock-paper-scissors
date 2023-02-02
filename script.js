a2f883df339e24e0d0abc86d6461f01b929f8b3f
Kago <kmotlhabane769@gmail.com>
2023-02-02 11:14:09 +0200

    Identify infinite loop problem with comments

--- script.js
+++ script.js
@@ -45,20 +45,32 @@ function game() {
         }
     };
 
+    playerWinCount++;
     let computerSelection = getComputerChoice();  
 
     //onClick play a round with parameters of playerSelection and computerSelection
-    playButtons.forEach(button => button.addEventListener('click', (e) => {
+    playButtons.forEach(button => {
+        button.addEventListener('click', (e) => {
+        playerWinCount++;
         let playerSelection = e.target.value;
-        return console.log(playRound(playerSelection, computerSelection)) } )
-    );
+        return console.log(playRound(playerSelection, computerSelection)); 
+        console.log(e.target);
+        });
+    });
+
 }
+
+// you're running the game function even before the user has done anything
+// but all I'm doing is adding event listeners, yes but I'm doing that in a loop with no action from a user
+// the count is never actually incremented unless the button is clicked
+// putting game() in a loop is the fatal flaw, as it repeatedly adds event listeners to buttons but nothing ever actually changes or terminates the loop
+// do not add event listeners in a while loop, just leave it to a regular counter to pick up on it. Don't click buttons in a loop. 
+// rather disable the event listeners 
 
 do {
     game();
     console.log(playerWinCount);
     console.log(computerWinCount);
-    playerWinCount++;
 } while (playerWinCount < 5 && computerWinCount < 5);
 
 if (playerWinCount === computerWinCount) {
