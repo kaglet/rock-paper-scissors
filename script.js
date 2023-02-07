657d270e87398f1a905942535c19dabd9fd421ad
Kago <kmotlhabane769@gmail.com>
2023-02-07 23:05:56 +0200

    Fix add typo to remove class on mouseout event

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function addPlayerButtonsHoverListeners() {
 
     playButtons.forEach(button => {
         button.addEventListener('mouseover', button.classList.add('hover'));
-        button.addEventListener('mouseout', button.classList.add('hover'));
+        button.addEventListener('mouseout', button.classList.remove('hover'));
     });
 }
 
